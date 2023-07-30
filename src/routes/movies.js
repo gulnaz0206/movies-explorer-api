bd5f0badd80d7fc55abca860a63fc95a4dc08095
Gulnaz Nasrtdinova <gulnaz0206@yandex.ru>
2023-07-30 13:43:58 +0500

    Удалила файлы и папки из ветки main

--- src/routes/movies.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const { getUsersMovies, createMovie, deleteMovie } = require('../controllers/movies');
-const { movieValidation, movieIdValidation } = require('../middlewares/validation');
-
-router.get('/', getUsersMovies);
-router.post('/', movieValidation, createMovie);
-router.delete('/:movieId', movieIdValidation, deleteMovie);
-
-module.exports = router;
